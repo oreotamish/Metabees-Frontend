84494dfcb0dd7a996f0bc54761a47be55f36cd8f
crazyhaller <suvigyamadrid@gmail.com>
2023-05-26 19:14:18 +0530

    edit product and about content

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -8,14 +8,21 @@ export default function Products() {
           alt=""
         />
         <div className=" ml-2 flex flex-col" style={{ marginTop: '100px' }}>
-          <h3 className="text-[#864bc9] text-2xl font-bold mb-4 home-heading">
-            Products
+          <h3 className="text-[#864bc9] text-2xl font-bold mb-5 home-heading">
+            Neptaverse
           </h3>
           <p className="text-white">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-            sunt nam perferendis doloremque placeat ullam, ab cumque alias
-            iusto. Ea laboriosam nam facilis sed, eum ducimus quidem quae
-            repudiandae doloribus.
+            Harnessing the immense capability of virtual reality is the core
+            purpose of our product. The Neptaverse brings brands to target
+            audiences and larger demographics.
+            <br />
+            <br />
+            Our subscribers can showcase their products in virtual worlds that
+            will in turn be made available to their customers.
+            <br />
+            <br />
+            Equipped with an oculus and a corresponding mobile device, one can
+            access these worlds from the comfort of their home.
           </p>
         </div>
       </div>
